21d0cdc3f70c3f9147ebc59580bfad849bc85e26
Rodney <rodney.amani@student.moringaschool.com>
2025-04-12 12:33:44 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import Home from "./components/Home";
+import About from "./components/About";
+import NavBar from "./components/NavBar";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
